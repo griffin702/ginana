f636895e2fa40aeec9ded898642ec9fc788630fa
griffin702 <117976509@qq.com>
2020-04-07 23:44:59 +0800

    新增ecode

---
+++ library/ecode/ecode.go
@@ -0,0 +1,70 @@
+package ecode
+
+import (
+	"fmt"
+	"github.com/pkg/errors"
+	"strconv"
+	"strings"
+	"sync/atomic"
+)
+
+var (
+	_messages atomic.Value // NOTE: stored map[int]string
+)
+
+// Register register ecode message map.
+func Register(cm map[int]string) {
+	_messages.Store(cm)
+}
+
+type ECode interface {
+	// sometimes Error return Code in string form
+	// NOTE: don't use Error in monitor report even it also work for now
+	Error() string
+	// Code get error code.
+	Code() int
+	// Message get code message.
+	Message() string
+}
+
+type ecode struct {
+	code    int
+	message string
+}
+
+func (e *ecode) Error() string {
+	return e.message
+}
+
+func (e *ecode) Code() int {
+	return e.code
+}
+
+func (e *ecode) Message() string {
+	if cm, ok := _messages.Load().(map[int]string); ok {
+		if msg, ok := cm[e.Code()]; ok {
+			return msg
+		}
+	}
+	if e.message == "" {
+		return strconv.Itoa(e.Code())
+	}
+	return e.message
+}
+
+func Errorf(code int, args ...interface{}) *ecode {
+	message := ""
+	if len(args) > 0 {
+		message = fmt.Sprintf(strings.Repeat("%v ", len(args)), args...)
+	}
+	return &ecode{code: code, message: message}
+}
+
+// Cause cause from error to ecode.
+func Cause(e error) ECode {
+	ec, ok := errors.Cause(e).(ECode)
+	if ok {
+		return ec
+	}
+	return &ecode{code: 500, message: e.Error()}
+}
