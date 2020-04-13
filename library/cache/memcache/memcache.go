0e5b395e809778f44699d67052876446e6b2de23
griffin702 <117976509@qq.com>
2020-04-13 11:48:47 +0800

    新增memcache

--- library/cache/memcache/memcache.go
+++ library/cache/memcache/memcache.go
@@ -25,6 +25,16 @@ func New(cfg *Config) Memcache {
 type Memcache interface {
 	Get(key string, obj interface{}) (err error)
 	Set(key string, v interface{}) (err error)
+	Add(key string, v interface{}) (err error)
+	Replace(key string, v interface{}) (err error)
+	GetMulti(keys []string) (items map[string]*mc.Item, err error)
+	Touch(key string, seconds int32) (err error)
+	Increment(key string, delta uint64) (newValue uint64, err error)
+	Decrement(key string, delta uint64) (newValue uint64, err error)
+	DeleteAll() (err error)
+	Delete(key string) (err error)
+	FlushAll() (err error)
+	CompareAndSwap(item *mc.Item) (err error)
 }
 
 // Memcache memcache client
@@ -37,8 +47,16 @@ func (m *memcache) decode(item *mc.Item, v interface{}) (err error) {
 	if rv.Kind() != reflect.Ptr || rv.IsNil() {
 		return &InvalidUnmarshalError{reflect.TypeOf(v)}
 	}
-	if item != nil {
-		err = json.Unmarshal(item.Value, v)
+	data := item.Value
+	switch v.(type) {
+	case *[]byte:
+		d := v.(*[]byte)
+		*d = data
+	case *string:
+		d := v.(*string)
+		*d = string(data)
+	case interface{}:
+		err = json.Unmarshal(data, v)
 	}
 	return
 }
@@ -67,5 +85,63 @@ func (m *memcache) Set(key string, v interface{}) (err error) {
 		return
 	}
 	err = m.client.Set(&mc.Item{Key: key, Value: value})
+	return
+}
+
+func (m *memcache) Add(key string, v interface{}) (err error) {
+	value, err := m.encode(v)
+	if err != nil {
+		return
+	}
+	err = m.client.Add(&mc.Item{Key: key, Value: value})
+	return
+}
+
+func (m *memcache) Replace(key string, v interface{}) (err error) {
+	value, err := m.encode(v)
+	if err != nil {
+		return
+	}
+	err = m.client.Replace(&mc.Item{Key: key, Value: value})
+	return
+}
+
+func (m *memcache) GetMulti(keys []string) (items map[string]*mc.Item, err error) {
+	items, err = m.client.GetMulti(keys)
+	return
+}
+
+func (m *memcache) Touch(key string, seconds int32) (err error) {
+	err = m.client.Touch(key, seconds)
+	return
+}
+
+func (m *memcache) Increment(key string, delta uint64) (newValue uint64, err error) {
+	newValue, err = m.client.Increment(key, delta)
+	return
+}
+
+func (m *memcache) Decrement(key string, delta uint64) (newValue uint64, err error) {
+	newValue, err = m.client.Decrement(key, delta)
+	return
+}
+
+func (m *memcache) DeleteAll() (err error) {
+	err = m.client.DeleteAll()
+	return
+}
+
+func (m *memcache) Delete(key string) (err error) {
+	err = m.client.Delete(key)
+	return
+}
+
+func (m *memcache) FlushAll() (err error) {
+	err = m.client.FlushAll()
+	return
+}
+
+func (m *memcache) CompareAndSwap(item *mc.Item) (err error) {
+	err = m.client.CompareAndSwap(item)
 	return
 }
