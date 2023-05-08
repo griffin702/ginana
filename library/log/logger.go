c1e964a64e811d810218aedb6d90b1ed1283224a
wuyun <117976509@qq.com>
2023-05-08 14:22:21 +0800

    debug

--- library/log/logger.go
+++ library/log/logger.go
@@ -47,7 +47,7 @@ func Errorf(format string, args ...interface{}) {
 }
 
 func Warnf(format string, args ...interface{}) {
-	log.Infof(format, args...)
+	log.Warnf(format, args...)
 }
 
 func Infof(format string, args ...interface{}) {
@@ -55,5 +55,5 @@ func Infof(format string, args ...interface{}) {
 }
 
 func Debugf(format string, args ...interface{}) {
-	log.Infof(format, args...)
+	log.Debugf(format, args...)
 }
